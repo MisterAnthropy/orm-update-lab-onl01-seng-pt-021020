ce2f5152198ad491c6a510a3eb6872d36c121cea
Tyler Croley <tjcroley@outlook.com>
2020-04-23 18:07:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,24 @@ class Student
   def self.drop_table
     DB[:conn].execute("DROP TABLE IF EXISTS students")
   end
-    
+  
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+      SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+  
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
 
 
 end
