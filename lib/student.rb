102358eb36bece45fa4a0fc50f8a683545c1fa6e
Tyler Croley <tjcroley@outlook.com>
2020-04-23 18:10:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,14 +36,23 @@ class Student
       SQL
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
   end
-end
   
   def self.create(name, grade)
     student = Student.new(name, grade)
     student.save
     student
   end
-
+  
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+  
+  
 
 end
